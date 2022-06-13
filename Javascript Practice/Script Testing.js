350201e57981b87fdf81471a21fe5272aabeba19
Aman Kapoor <amankapoor140@gmail.com>
2022-06-13 22:34:32 +0530

    Update Script Testing.js

--- Javascript Practice/Script Testing.js
+++ Javascript Practice/Script Testing.js
@@ -223,22 +223,7 @@ alert
 /*
 
 
-//................Remove_duplicate_item_from_an_array.............//
-function Remove_duplicate_item_from_an_array()
-{
-    var n = [];
-for (let i = 0; i < 5; i++) {
 
-    n[i] = prompt("Enter Array Element " + (n));
-
-}
-let uniqueChars = [...new Set(n)];
-
-console.log(uniqueChars);
-document.write("Your Array is " + n);
-document.write("<br/> <br/>");
-document.write("New Array after removing duplicate item:- " + uniqueChars);
-}
 
 
 //.............factorial_of_a_Number..............//
